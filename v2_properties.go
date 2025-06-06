75e724b7f8fab9c2c423e7f12e6a30cead0cc3c3
Laurens Bremers <laurens@bremersklaver.nl>
2025-06-06 12:41:46 +0200

    change package name

--- v2_properties.go
+++ v2_properties.go
@@ -1,4 +1,7 @@
-package schema
+//go:generate go install github.com/atombender/go-jsonschema@v0.20.0
+//go:generate go run github.com/atombender/go-jsonschema@v0.20.0 --schema-package=v2_layer_properties=github.com/schoolyear/avdimagetypes --schema-output=v2_layer_properties=v2_properties_types.go v2_properties.json
+
+package avdimagetypes
 
 import (
 	_ "embed"
